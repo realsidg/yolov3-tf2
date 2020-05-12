e75e6ecbf22511a2b7db5ebe1df2e0ce2b3004dd
realsidg <i@siddharthgarg.in>
2020-05-13 01:38:51 +0530

    TPU optimized training

--- train.py
+++ train.py
@@ -45,134 +45,143 @@ flags.DEFINE_integer('weights_num_classes', None, 'specify num class for `weight
 
 
 def main(_argv):
-    physical_devices = tf.config.experimental.list_physical_devices('GPU')
-    for physical_device in physical_devices:
-        tf.config.experimental.set_memory_growth(physical_device, True)
+    # physical_devices = tf.config.experimental.list_physical_devices('GPU')
+    # for physical_device in physical_devices:
+        # tf.config.experimental.set_memory_growth(physical_device, True)
+    # detect and init the TPU
+    tpu = tf.distribute.cluster_resolver.TPUClusterResolver()
+    tf.config.experimental_connect_to_cluster(tpu)
+    tf.tpu.experimental.initialize_tpu_system(tpu)
 
-    if FLAGS.tiny:
-        model = YoloV3Tiny(FLAGS.size, training=True,
-                           classes=FLAGS.num_classes)
-        anchors = yolo_tiny_anchors
-        anchor_masks = yolo_tiny_anchor_masks
-    else:
-        model = YoloV3(FLAGS.size, training=True, classes=FLAGS.num_classes)
-        anchors = yolo_anchors
-        anchor_masks = yolo_anchor_masks
+    # instantiate a distribution strategy
+    tpu_strategy = tf.distribute.experimental.TPUStrategy(tpu)
 
-    train_dataset = dataset.load_fake_dataset()
-    if FLAGS.dataset:
-        train_dataset = dataset.load_tfrecord_dataset(
-            FLAGS.dataset, FLAGS.classes, FLAGS.size)
-    train_dataset = train_dataset.shuffle(buffer_size=512)
-    train_dataset = train_dataset.batch(FLAGS.batch_size)
-    train_dataset = train_dataset.map(lambda x, y: (
-        dataset.transform_images(x, FLAGS.size),
-        dataset.transform_targets(y, anchors, anchor_masks, FLAGS.size)))
-    train_dataset = train_dataset.prefetch(
-        buffer_size=tf.data.experimental.AUTOTUNE)
-
-    val_dataset = dataset.load_fake_dataset()
-    if FLAGS.val_dataset:
-        val_dataset = dataset.load_tfrecord_dataset(
-            FLAGS.val_dataset, FLAGS.classes, FLAGS.size)
-    val_dataset = val_dataset.batch(FLAGS.batch_size)
-    val_dataset = val_dataset.map(lambda x, y: (
-        dataset.transform_images(x, FLAGS.size),
-        dataset.transform_targets(y, anchors, anchor_masks, FLAGS.size)))
-
-    # Configure the model for transfer learning
-    if FLAGS.transfer == 'none':
-        pass  # Nothing to do
-    elif FLAGS.transfer in ['darknet', 'no_output']:
-        # Darknet transfer is a special case that works
-        # with incompatible number of classes
-
-        # reset top layers
+    # instantiating the model in the strategy scope creates the model on the TPU
+    with tpu_strategy.scope():
         if FLAGS.tiny:
-            model_pretrained = YoloV3Tiny(
-                FLAGS.size, training=True, classes=FLAGS.weights_num_classes or FLAGS.num_classes)
+            model = YoloV3Tiny(FLAGS.size, training=True,
+                            classes=FLAGS.num_classes)
+            anchors = yolo_tiny_anchors
+            anchor_masks = yolo_tiny_anchor_masks
         else:
-            model_pretrained = YoloV3(
-                FLAGS.size, training=True, classes=FLAGS.weights_num_classes or FLAGS.num_classes)
-        model_pretrained.load_weights(FLAGS.weights)
+            model = YoloV3(FLAGS.size, training=True, classes=FLAGS.num_classes)
+            anchors = yolo_anchors
+            anchor_masks = yolo_anchor_masks
 
-        if FLAGS.transfer == 'darknet':
-            model.get_layer('yolo_darknet').set_weights(
-                model_pretrained.get_layer('yolo_darknet').get_weights())
-            freeze_all(model.get_layer('yolo_darknet'))
+        train_dataset = dataset.load_fake_dataset()
+        if FLAGS.dataset:
+            train_dataset = dataset.load_tfrecord_dataset(
+                FLAGS.dataset, FLAGS.classes, FLAGS.size)
+        train_dataset = train_dataset.shuffle(buffer_size=512)
+        train_dataset = train_dataset.batch(FLAGS.batch_size)
+        train_dataset = train_dataset.map(lambda x, y: (
+            dataset.transform_images(x, FLAGS.size),
+            dataset.transform_targets(y, anchors, anchor_masks, FLAGS.size)))
+        train_dataset = train_dataset.prefetch(
+            buffer_size=tf.data.experimental.AUTOTUNE)
 
-        elif FLAGS.transfer == 'no_output':
-            for l in model.layers:
-                if not l.name.startswith('yolo_output'):
-                    l.set_weights(model_pretrained.get_layer(
-                        l.name).get_weights())
-                    freeze_all(l)
+        val_dataset = dataset.load_fake_dataset()
+        if FLAGS.val_dataset:
+            val_dataset = dataset.load_tfrecord_dataset(
+                FLAGS.val_dataset, FLAGS.classes, FLAGS.size)
+        val_dataset = val_dataset.batch(FLAGS.batch_size)
+        val_dataset = val_dataset.map(lambda x, y: (
+            dataset.transform_images(x, FLAGS.size),
+            dataset.transform_targets(y, anchors, anchor_masks, FLAGS.size)))
 
-    else:
-        # All other transfer require matching classes
-        model.load_weights(FLAGS.weights)
-        if FLAGS.transfer == 'fine_tune':
-            # freeze darknet and fine tune other layers
-            darknet = model.get_layer('yolo_darknet')
-            freeze_all(darknet)
-        elif FLAGS.transfer == 'frozen':
-            # freeze everything
-            freeze_all(model)
+        # Configure the model for transfer learning
+        if FLAGS.transfer == 'none':
+            pass  # Nothing to do
+        elif FLAGS.transfer in ['darknet', 'no_output']:
+            # Darknet transfer is a special case that works
+            # with incompatible number of classes
 
-    optimizer = tf.keras.optimizers.Adam(lr=FLAGS.learning_rate)
-    loss = [YoloLoss(anchors[mask], classes=FLAGS.num_classes)
-            for mask in anchor_masks]
+            # reset top layers
+            if FLAGS.tiny:
+                model_pretrained = YoloV3Tiny(
+                    FLAGS.size, training=True, classes=FLAGS.weights_num_classes or FLAGS.num_classes)
+            else:
+                model_pretrained = YoloV3(
+                    FLAGS.size, training=True, classes=FLAGS.weights_num_classes or FLAGS.num_classes)
+            model_pretrained.load_weights(FLAGS.weights)
 
-    if FLAGS.mode == 'eager_tf':
-        # Eager mode is great for debugging
-        # Non eager graph mode is recommended for real training
-        avg_loss = tf.keras.metrics.Mean('loss', dtype=tf.float32)
-        avg_val_loss = tf.keras.metrics.Mean('val_loss', dtype=tf.float32)
+            if FLAGS.transfer == 'darknet':
+                model.get_layer('yolo_darknet').set_weights(
+                    model_pretrained.get_layer('yolo_darknet').get_weights())
+                freeze_all(model.get_layer('yolo_darknet'))
 
-        for epoch in range(1, FLAGS.epochs + 1):
-            for batch, (images, labels) in enumerate(train_dataset):
-                with tf.GradientTape() as tape:
-                    outputs = model(images, training=True)
+            elif FLAGS.transfer == 'no_output':
+                for l in model.layers:
+                    if not l.name.startswith('yolo_output'):
+                        l.set_weights(model_pretrained.get_layer(
+                            l.name).get_weights())
+                        freeze_all(l)
+
+        else:
+            # All other transfer require matching classes
+            model.load_weights(FLAGS.weights)
+            if FLAGS.transfer == 'fine_tune':
+                # freeze darknet and fine tune other layers
+                darknet = model.get_layer('yolo_darknet')
+                freeze_all(darknet)
+            elif FLAGS.transfer == 'frozen':
+                # freeze everything
+                freeze_all(model)
+
+        optimizer = tf.keras.optimizers.Adam(lr=FLAGS.learning_rate)
+        loss = [YoloLoss(anchors[mask], classes=FLAGS.num_classes)
+                for mask in anchor_masks]
+
+        if FLAGS.mode == 'eager_tf':
+            # Eager mode is great for debugging
+            # Non eager graph mode is recommended for real training
+            avg_loss = tf.keras.metrics.Mean('loss', dtype=tf.float32)
+            avg_val_loss = tf.keras.metrics.Mean('val_loss', dtype=tf.float32)
+
+            for epoch in range(1, FLAGS.epochs + 1):
+                for batch, (images, labels) in enumerate(train_dataset):
+                    with tf.GradientTape() as tape:
+                        outputs = model(images, training=True)
+                        regularization_loss = tf.reduce_sum(model.losses)
+                        pred_loss = []
+                        for output, label, loss_fn in zip(outputs, labels, loss):
+                            pred_loss.append(loss_fn(label, output))
+                        total_loss = tf.reduce_sum(pred_loss) + regularization_loss
+
+                    grads = tape.gradient(total_loss, model.trainable_variables)
+                    optimizer.apply_gradients(
+                        zip(grads, model.trainable_variables))
+
+                    logging.info("{}_train_{}, {}, {}".format(
+                        epoch, batch, total_loss.numpy(),
+                        list(map(lambda x: np.sum(x.numpy()), pred_loss))))
+                    avg_loss.update_state(total_loss)
+
+                for batch, (images, labels) in enumerate(val_dataset):
+                    outputs = model(images)
                     regularization_loss = tf.reduce_sum(model.losses)
                     pred_loss = []
                     for output, label, loss_fn in zip(outputs, labels, loss):
                         pred_loss.append(loss_fn(label, output))
                     total_loss = tf.reduce_sum(pred_loss) + regularization_loss
 
-                grads = tape.gradient(total_loss, model.trainable_variables)
-                optimizer.apply_gradients(
-                    zip(grads, model.trainable_variables))
+                    logging.info("{}_val_{}, {}, {}".format(
+                        epoch, batch, total_loss.numpy(),
+                        list(map(lambda x: np.sum(x.numpy()), pred_loss))))
+                    avg_val_loss.update_state(total_loss)
 
-                logging.info("{}_train_{}, {}, {}".format(
-                    epoch, batch, total_loss.numpy(),
-                    list(map(lambda x: np.sum(x.numpy()), pred_loss))))
-                avg_loss.update_state(total_loss)
+                logging.info("{}, train: {}, val: {}".format(
+                    epoch,
+                    avg_loss.result().numpy(),
+                    avg_val_loss.result().numpy()))
 
-            for batch, (images, labels) in enumerate(val_dataset):
-                outputs = model(images)
-                regularization_loss = tf.reduce_sum(model.losses)
-                pred_loss = []
-                for output, label, loss_fn in zip(outputs, labels, loss):
-                    pred_loss.append(loss_fn(label, output))
-                total_loss = tf.reduce_sum(pred_loss) + regularization_loss
-
-                logging.info("{}_val_{}, {}, {}".format(
-                    epoch, batch, total_loss.numpy(),
-                    list(map(lambda x: np.sum(x.numpy()), pred_loss))))
-                avg_val_loss.update_state(total_loss)
-
-            logging.info("{}, train: {}, val: {}".format(
-                epoch,
-                avg_loss.result().numpy(),
-                avg_val_loss.result().numpy()))
-
-            avg_loss.reset_states()
-            avg_val_loss.reset_states()
-            model.save_weights(
-                'checkpoints/yolov3_train_{}.tf'.format(epoch))
-    else:
-        model.compile(optimizer=optimizer, loss=loss,
-                      run_eagerly=(FLAGS.mode == 'eager_fit'))
+                avg_loss.reset_states()
+                avg_val_loss.reset_states()
+                model.save_weights(
+                    'checkpoints/yolov3_train_{}.tf'.format(epoch))
+        else:
+            model.compile(optimizer=optimizer, loss=loss,
+                        run_eagerly=(FLAGS.mode == 'eager_fit'))
 
         callbacks = [
             ReduceLROnPlateau(verbose=1),
